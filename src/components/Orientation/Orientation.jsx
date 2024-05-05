98a7d98420f5f9dfae2b8287581c80dd20a41b22
Mereke Kuanysh <kz9merek@gmail.com>
2024-05-05 22:13:01 +0500

    PlaneGrid

--- src/components/Orientation/Orientation.jsx
+++ src/components/Orientation/Orientation.jsx
@@ -1,32 +1,10 @@
 import ArtificialHorizon from "../ArtificialHorizon/ArtificialHorizon";
 import Compass from "../Compass/Compass";
+import PlaneGrid from "../PlaneGrid/PlaneGrid";
 import Rbi from "../Rbi/Rbi";
 import styles from "./Orientation.module.css";
 
-function Beacon() {
-    return (
-        <div className={styles.beacon}>
-            <div></div>
-            <div></div>
-        </div>
-    );
-}
-
 function Orientation() {
-    const cells = [];
-    const rows = 5;
-    const columns = 5;
-
-    for (let i = 0; i < rows * columns; i++) {
-        let content = null;
-
-        if (i === 12) {
-            content = <Beacon />; 
-        } 
-
-        cells.push(<div className={styles.cell}>{content}</div>);
-    }
-
     return (
         <div className={styles.content}>
             <div className={styles.instruments}>
@@ -34,9 +12,7 @@ function Orientation() {
                 <ArtificialHorizon />
                 <Compass />
             </div>
-            <div className={styles.grid}>
-                {cells}
-            </div>
+            <PlaneGrid />
         </div>
     );
 }
